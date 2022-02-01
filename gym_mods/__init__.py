9bbfa5852eac65f85dcf63b82ff511ef352fce2a
Filip Skubacz <filip.skubacz00@gmail.com>
2022-02-01 20:20:55 +0100

    Add register envs with user-defined names

--- gym_mods/__init__.py
+++ gym_mods/__init__.py
@@ -3,19 +3,19 @@ import gym
 from gym_mods.wrappers import *
 
 
-def register_cartpole(max_length: int = 3000):
+def register_cartpole(max_length: int = 3000, name: str = "CartPole-v2"):
     assert max_length > 0
     gym.envs.registration.register(
-        id="CartPole-v3",
+        id=name,
         entry_point="gym.envs.classic_control:CartPoleEnv",
         max_episode_steps=int(max_length),
         reward_threshold=float(max_length),
     )
 
 
-def register_infinite_cartpole():
+def register_infinite_cartpole(name: str = "CartPole-v3"):
     gym.envs.registration.register(
-        id="CartPole-v4",
+        id=name,
         entry_point="gym.envs.classic_control:CartPoleEnv",
         max_episode_steps=None,
         reward_threshold=float("inf"),
